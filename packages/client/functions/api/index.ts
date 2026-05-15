95177069fd05c5ec96d12158e92c9107c86804ae
Iain Galloway <iain.galloway@gmail.com>
2026-05-15 21:17:16 +0000

    Fix missing version in client

--- packages/client/functions/api/index.ts
+++ packages/client/functions/api/index.ts
@@ -3,13 +3,14 @@ import { greet } from "@spike/shared";
 interface Env {
   VERSION: string;
   KV: KVNamespace;
+  CF_PAGES_COMMIT_SHA?: string;
 }
 
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   const kv = await context.env.KV.get("spike-test");
   return Response.json({
     message: greet("world"),
-    version: context.env.VERSION,
+    version: context.env.CF_PAGES_COMMIT_SHA || context.env.VERSION,
     kv
   });
 };
